95eb3e291c9081fc625ae9a9920bb98ded90a946
dylan <dylandiaz101@yahoo.com>
2022-10-11 11:32:54 -0700

    update comments and code consistency

--- text_to_speech.py
+++ text_to_speech.py
@@ -2,12 +2,13 @@ import pyttsx3
 
 engine = pyttsx3.init()
 
-rate = engine.getProperty('rate')
-engine.setProperty('rate', 150)
+# set speaking rate
+rate = engine.getProperty("rate")
+engine.setProperty("rate", 150)
 
-voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[1].id)
-
+# set voice to female
+voices = engine.getProperty("voices")
+engine.setProperty("voice", voices[1].id)
 
 def say(text):  # variable name shortcut
     engine.say(text)
