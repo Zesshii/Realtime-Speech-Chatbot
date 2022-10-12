de5fc02dcb44431e4b01e26a96cefd96e902a8c7
dylan <dylandiaz101@yahoo.com>
2022-10-12 10:36:31 -0700

    separate pre-config input and reformat

--- text_to_speech.py
+++ text_to_speech.py
@@ -10,6 +10,7 @@ engine.setProperty("rate", 150)
 voices = engine.getProperty("voices")
 engine.setProperty("voice", voices[1].id)
 
+
 def say(text):  # variable name shortcut
     engine.say(text)
     engine.runAndWait()
